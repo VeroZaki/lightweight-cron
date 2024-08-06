4b94b5d7f152df30c3608909b5c5490550b74335
Veronia Osama <veronia.osama@bosta.co>
2024-08-06 03:11:07 +0300

    feat: add start worker manager in main

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import initializeApp from './express/index.js';
 import initiateMongoConnection from './lib/mongoose.js';
+import WorkerManager from './cronHandler/workers/workerManager.js'
 
 (async () => {
   await initiateMongoConnection();
   initializeApp();
+  await WorkerManager.start();
+
 })();
