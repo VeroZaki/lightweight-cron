66fe5cfeb7120f1d592a4a7d1811859ead5babcb
Veronia Osama <veronia.osama@bosta.co>
2024-08-07 20:23:32 +0300

    feat: add task2 with same cron expression as task3

--- tasks/index.js
+++ tasks/index.js
@@ -1,12 +1,9 @@
 import Task1 from './task1/index.js';
 import Task3 from './repeatable/task3/index.js';
-
-// import BusinessTasks from './repeatable/business/index';
-// import tasksOps from './repeatable/tasksOps';
-// import OmniChannelTasks from './priority/omniChannel/index';
-// import DomainsTasks from './repeatable/domains/index';
+import Task2 from './repeatable/task2/index.js';
 
 export default {
   Task1_runner: Task1,
-  Task3: Task3
+  Task3: Task3,
+  Task2: Task2
 };
