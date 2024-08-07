d90ac4c1b327345ab98253a27cc9c253fc4de083
Veronia Osama <veronia.osama@bosta.co>
2024-08-07 14:52:17 +0300

    feat: add test tasks

---
+++ tasks/index.js
@@ -0,0 +1,12 @@
+import Task1 from './task1/index.js';
+import Task3 from './repeatable/task3/index.js';
+
+// import BusinessTasks from './repeatable/business/index';
+// import tasksOps from './repeatable/tasksOps';
+// import OmniChannelTasks from './priority/omniChannel/index';
+// import DomainsTasks from './repeatable/domains/index';
+
+export default {
+  Task1_runner: Task1,
+  Task3: Task3
+};
---
+++ tasks/repeatable/task3/index.js
@@ -0,0 +1,14 @@
+class Task3 {
+    run(args) {
+      console.log('holaaaaaaaaaaaaaaaaa Task 3', args);
+      return `Hello ${args}`;
+    }
+  
+    run2(args) {
+      // console.log('holaaaaaaaaaaaaaaaaa222', args);
+      return `Hello2 ${args}`;
+    }
+  }
+  
+  export default new Task3();
+  
